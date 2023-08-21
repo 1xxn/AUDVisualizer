2bb948a596181b4e75bbc82efd0e0ed868b65690
1xxn <leon@segebart.de>
2023-08-21 17:51:35 +0200

    Initial Commit

---
+++ AUDVisualizer/AUDVisualizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AUDVisualizerApp.swift
+//  AUDVisualizer
+//
+//  Created by Leon on 21.08.23.
+//
+
+import SwiftUI
+
+@main
+struct AUDVisualizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
